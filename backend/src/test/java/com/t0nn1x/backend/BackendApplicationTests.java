b134656c74b678e706ead7613cc42e04e2ed2b21
t0nn1x <antoonhrobust@gmail.com>
2023-08-16 20:20:46 +0200

    Created spring boot project for backend

---
+++ backend/src/test/java/com/t0nn1x/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t0nn1x.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
