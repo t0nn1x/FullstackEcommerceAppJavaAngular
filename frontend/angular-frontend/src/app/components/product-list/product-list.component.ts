71733552d85ab3588a76f30c5c61555224723f98
t0nn1x <antoonhrobust@gmail.com>
2023-08-17 12:24:31 +0200

    Updated product-list.component(for reading data from API)

--- frontend/angular-frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-frontend/src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {Product} from "../../common/product";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+    // Create an array of type Product
+    products: Product[] = [];
 
-  ngOnInit(): void {
-  }
+    // Inject the ProductService
+    constructor(private productService: ProductService) {
+    }
+
+    // Call the listProducts() method when the component is initialized
+    ngOnInit(): void {
+        this.listProducts();
+    }
+
+    listProducts() {
+        this.productService.getProductList().subscribe(
+            data => {
+                this.products = data;
+            }
+        )
+    }
 
 }
