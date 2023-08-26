e817d7ed3f9f460c3ffce65e0579c4fe4a35f9a1
t0nn1x <antoonhrobust@gmail.com>
2023-08-26 19:33:25 +0200

    Updated product list component for pagination

--- frontend/angular-frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-frontend/src/app/components/product-list/product-list.component.ts
@@ -4,77 +4,98 @@ import {Product} from "../../common/product";
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
-    selector: 'app-product-list',
-    templateUrl: './product-list-grid.component.html',
-    styleUrls: ['./product-list.component.css']
+  selector: 'app-product-list',
+  templateUrl: './product-list-grid.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
 
-    products: Product[] = [];
-    currentCategoryId: number = 1;
-    currentCategoryName: string = "";
-    searchMode: boolean = false;
+  products: Product[] = [];
+  previousCategoryId: number = 1;
+  currentCategoryId: number = 1;
+  currentCategoryName: string = "";
+  searchMode: boolean = false;
 
+  // New props for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
 
-    // Inject the ProductService
-    constructor(private productService: ProductService,
-                private route: ActivatedRoute) {
+  // Inject the ProductService
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {
+  }
+
+  // Call the listProducts() method when the component is initialized
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
+    this.listProducts();
+  }
+
+  listProducts() {
+
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+  }
+
+  handleListProducts() {
+    //check if id parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
+    } else {
+      // not category id available ... default to category id 1
+      this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
-    // Call the listProducts() method when the component is initialized
-    ngOnInit(): void {
-        this.route.paramMap.subscribe(() => {
-            this.listProducts();
-        });
-        this.listProducts();
+    // check if we have a different category than previous
+
+    // if we have a different category id than previous,
+    // then set thePageNumber back to 1
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
     }
 
-    listProducts() {
+    this.previousCategoryId = this.currentCategoryId;
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
 
-        this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    // now get the products for the given category id
+    this.productService.getProductListPaginate(
+      this.thePageNumber - 1,
+      this.thePageSize,
+      this.currentCategoryId
+    ).subscribe(data => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    });
+  }
 
-        if(this.searchMode){
-            this.handleSearchProducts();
-        } else {
-            this.handleListProducts();
-        }
-    }
+  private handleSearchProducts() {
+    const theKeyword: string | null = this.route.snapshot.paramMap.get('keyword');
 
-    handleListProducts() {
-        //check if id parameter is available
-        const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    // now search for products using given keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
 
-        if (hasCategoryId) {
-            // get the "id" param string. convert string to a number using the "+" symbol
-            this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
-
-            // get the "name" param string
-            this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
-        } else {
-            // not category id available ... default to category id 1
-            this.currentCategoryId = 1;
-            this.currentCategoryName = 'Books';
-        }
-
-        // now get the products for the given category id
-        this.productService.getProductList(this.currentCategoryId).subscribe(
-            data => {
-                this.products = data;
-            }
-        )
-    }
-
-    private handleSearchProducts() {
-      const theKeyword: string | null = this.route.snapshot.paramMap.get('keyword');
-
-      // now search for products using given keyword
-      this.productService.searchProducts(theKeyword).subscribe(
-        data => {
-          this.products = data;
-        }
-      );
-    }
-
-    protected readonly length = length;
+  protected readonly length = length;
 }
